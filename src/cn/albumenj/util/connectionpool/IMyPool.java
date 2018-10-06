fc2fd2811c6b495e507ddc0e40e29caa997b2bb4
Albumen <jhq0812@hotmail.com>
2018-10-06 16:22:31 +0800

    进行了一点优化

--- src/cn/albumenj/util/connectionpool/IMyPool.java
+++ src/cn/albumenj/util/connectionpool/IMyPool.java
@@ -1,8 +1,5 @@
 package cn.albumenj.util.connectionpool;
 
-/**
- * @author Albumen
- */
 public interface IMyPool {
     PooledConnection getPooledConnection();
     void createPooledConnection(int count);
